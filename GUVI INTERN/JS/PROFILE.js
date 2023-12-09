9c23f451bdad9c56b2c9205185810fde2686dacd
Sheggari Tejasri <144826695+sheggariTejasri@users.noreply.github.com>
2023-12-09 18:28:45 +0530

    Delete GUVI INTERN directory

--- GUVI INTERN/JS/PROFILE.js
+++
@@ -1,6 +0,0 @@
-// profile.js
-function showPage(pageId) {
-    document.querySelectorAll('.page').forEach(page => page.style.display = 'none');
-    document.getElementById(pageId).style.display = 'block';
-  }
-  
--- GUVI INTERN/JS/register.js
+++
@@ -1,6 +0,0 @@
-// register.js
-function showPage(pageId) {
-    document.querySelectorAll('.page').forEach(page => page.style.display = 'none');
-    document.getElementById(pageId).style.display = 'block';
-  }
-  
